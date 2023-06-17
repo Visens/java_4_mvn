bb001aa3bc5c2fb60ae0108f48f62ba1bd05301a
Trainning Git <tandrew555@mail.ru>
2023-06-17 09:03:38 +0400

    First commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,7 @@
 public class Main {
-
+    public static void main(String[] args) {
+        BonusService service = new BonusService();
+        long bonus = service.calculate(500, true);
+        System.out.println("Ваши бонусные баллы: " + bonus);
+    }
 }
